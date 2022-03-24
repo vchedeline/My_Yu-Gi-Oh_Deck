c62d24d8a7968f8701243ef74106a4e834b42ea1
chedeline <vchedeline@gmail.com>
2022-03-23 23:04:00 -0400

    this is the beginning

---
+++ js/app.js
@@ -0,0 +1,18 @@
+console.log("Hello World")
+
+$("button").on("click", (evt) => {
+  evt.preventDefault();
+  $.ajax({
+    url: "https://db.ygoprodeck.com/api/v7/randomcard.php",
+  })
+  .then((data) => {
+    console.log(data);
+    $("img").remove();
+
+    const $img = $("<img>");
+    
+    $img.attr("src", data.card_images[0].image_url);
+    $img.attr("atl", data.name);
+    $img.prependTo(".container");
+  })
+});
