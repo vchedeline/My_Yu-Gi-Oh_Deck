fe00b0f760ffb8d7015aac22cc7563666ae697bc
chedeline <vchedeline@gmail.com>
2022-03-26 13:22:06 -0400

    testing 2

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ console.log("Hello World")
 const deckImgs = [];
 const deckPrices = [];
 const deckDatas = [];
-let mainDeckMonster, spellCard, trapCard, extraDeckMonster, marketPrice;
+let mainDeckMonster, spellCard, trapCard, extraDeckMonster, marketPrice, currentCardIndex;
 
 // Finding a main deck card
 const getCardData = (type) => {
@@ -13,10 +13,37 @@ const getCardData = (type) => {
   });
 }
 
+function displayCard(currentCardIndex) {
+  deckDatas[currentCardIndex]
+  .then((data) => {
+    console.log(data);
+    $("img").remove();
+    $("p").remove();
+    
+    const $img = $("<img>");
+    
+    $img.attr("src", data.data[0].card_images[0].image_url).attr("atl", data.name).appendTo(".card-slides");
+    
+    deckImgs.push(data.data[0].card_images[0].image_url)
+    
+    // displays card specs
+    $(".card-info h1").text(data.data[0].name);
+    $("<p>").text(`Type: ${data.data[0].type}`).appendTo("#details");
+    $("<p>").text(`Race: ${data.data[0].race}`).appendTo("#details");
+    $("<p>").text(`Level: ${data.data[0].level}`).appendTo("#details");
+    $("<p>").text(`Attribute: ${data.data[0].attribute}`).appendTo("#details");
+    $("<p>").text(`ATK: ${data.data[0].atk}`).appendTo("#details");
+    $("<p>").text(`DEF: ${data.data[0].def}`).appendTo("#details");
+    $("<p>").text(`Price on Amazon: $${data.data[0].card_prices[0].amazon_price}`).appendTo("#details");
+    $("<p id='desc'>").text(data.data[0].desc).appendTo("#details");
+  })
+}
 
 $("#submit").on("click", (evt) => {
   evt.preventDefault();
   
+  currentCardIndex = 0;
+
   $(".arrows").css("display", "flex");
 
   mainDeckMonster = $("#main-deck").val();
@@ -36,30 +63,7 @@ $("#submit").on("click", (evt) => {
   deckDatas.push(getCardData(trapCard));
   deckDatas.push(getCardData(extraDeckMonster));
 
-  deckDatas[1]
-  .then((data) => {
-    console.log(data);
-    $("img").remove();
-    $("p").remove();
+  displayCard(currentCardIndex);
     
-    const $img = $("<img>");
-    
-    $img.attr("src", data.data[0].card_images[0].image_url).attr("atl", data.name).appendTo(".card-slides");
-    
-    deckImgs.push(data.data[0].card_images[0].image_url)
-    deckPrices.push(data.data[0].card_prices[0].amazon_price)
-    
-    // displays card specs
-    $(".card-info h1").text(data.data[0].name);
-    $("<p>").text(`Type: ${data.data[0].type}`).appendTo("#details");
-    $("<p>").text(`Race: ${data.data[0].race}`).appendTo("#details");
-    $("<p>").text(`Level: ${data.data[0].level}`).appendTo("#details");
-    $("<p>").text(`Attribute: ${data.data[0].attribute}`).appendTo("#details");
-    $("<p>").text(`ATK: ${data.data[0].atk}`).appendTo("#details");
-    $("<p>").text(`DEF: ${data.data[0].def}`).appendTo("#details");
-    $("<p>").text(`Price on Amazon: $${data.data[0].card_prices[0].amazon_price}`).appendTo("#details");
-    $("<p id='desc'>").text(data.data[0].desc).appendTo("#details");
-  })
-
   console.log(deckImgs, deckPrices);
 });
